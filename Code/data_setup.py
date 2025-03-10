4dd2646d2f2f819c43335008a87f5c2ad63061fc
Hoang Nguyen <hn02010404@gmail.com>
2025-03-10 22:59:50 +0700

    add "extractzip" function

--- Code/data_setup.py
+++ Code/data_setup.py
@@ -2,6 +2,8 @@ import pandas as pd
 import torch
 import os 
 import numpy as np
+import zipfile
+import shutil
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
@@ -43,6 +45,7 @@ def preprocess(data: pd.DataFrame) -> pd.DataFrame:
     # Drop date and code column
     preprocessed_data.drop(["Date", "code"], axis = 1, inplace = True)
     return preprocessed_data
+
 class SPP_Dataset(Dataset):
     """A class inherited from torch.utils.data.Dataset for loading CSV file for Stock Price Prediction"""
     def __init__(self, path: os.path, target_col: str):
@@ -55,8 +58,8 @@ class SPP_Dataset(Dataset):
     def __getitem__(self, index):
         features = self.data.drop(self.target_col, axis = 1).select_dtypes('number')
         target = self.data[self.target_col]
-        return torch.tensor(features.iloc[index,:].values, dtype=torch.float32), torch.tensor(target[index], dtype=torch.float32)
-    
+        return torch.tensor(features.iloc[index,:].values, dtype=torch.float32), torch.tensor(target[index], dtype=torch.float32)  
+
 def create_dataloaders(
         train_path: os.path = None,
         test_path: os.path = None,
@@ -100,5 +103,84 @@ def create_dataloaders(
         return train_dataloaders
     else:
         return test_dataloaders 
+    
+def extractzip():
+    """Set up folder and data for training and testing"""
+    # Set up folder
+    folder = os.path.join("..", "Data")
+    train_folder = os.path.join(folder, "train")
+    test_folder = os.path.join(folder, "test")
+
+    # Check Data folder
+    if os.path.exists(folder) and os.path.isdir(folder):
+        print(f"[INFO] {folder} folder exists")
+    else:
+        print(f"[INFO] Did not find {folder}, creating one...")
+        os.mkdir(folder)
+
+    # Check train folder 
+    if os.path.exists(train_folder) and os.path.isdir(train_folder):
+        print(f"[INFO] {train_folder} folder exists")
+    else:
+        print(f"[INFO] Did not find {train_folder}, creating one...")
+        os.mkdir(train_folder)
+
+    # Check test folder
+    if os.path.exists(test_folder) and os.path.isdir(test_folder):
+        print(f"[INFO] {test_folder} folder exists")
+    else:
+        print(f"[INFO] Did not find {test_folder}, creating one...")
+        os.mkdir(test_folder)
+
+    # Extract zip file for training
+    with zipfile.ZipFile(folder + "/train.zip", "r") as zip_train:
+        print("[INFO] Unzipping training data")
+        zip_train.extractall(train_folder)
+
+    # Remove __MACOSX folder if it exists
+    macosx_folder = os.path.join(train_folder, "__MACOSX")
+    if os.path.exists(macosx_folder):
+        print("[INFO] Removing __MACOSX folder")
+        shutil.rmtree(macosx_folder)
+
+    # Move contents from "STOCK PRICE DATA" to train folder
+    stock_data_folder = os.path.join(train_folder, "STOCK PRICE DATA")
+    if os.path.exists(stock_data_folder):
+        print("[INFO] Moving files from 'STOCK PRICE DATA' to train folder")
+        # Move all files from stock_data_folder to train_folder
+        for file in os.listdir(stock_data_folder):
+            src = os.path.join(stock_data_folder, file)
+            dst = os.path.join(train_folder, file)
+            shutil.move(src, dst)
+        
+        # Remove the now empty "STOCK PRICE DATA" folder
+        print("[INFO] Removing empty 'STOCK PRICE DATA' folder")
+        shutil.rmtree(stock_data_folder)
+    
+    # Extract zip file for testing
+    with zipfile.ZipFile(folder + "/test.zip", "r") as zip_test:
+        print("[INFO] Unzipping training data")
+        zip_test.extractall(test_folder)
+
+    # Remove __MACOSX folder if it exists
+    macosx_folder = os.path.join(test_folder, "__MACOSX")
+    if os.path.exists(macosx_folder):
+        print("[INFO] Removing __MACOSX folder")
+        shutil.rmtree(macosx_folder)
+
+    # Move contents from "Data Test" to train folder
+    stock_data_folder = os.path.join(test_folder, "Data Test")
+    if os.path.exists(stock_data_folder):
+        print("[INFO] Moving files from 'Data Test' to train folder")
+        # Move all files from stock_data_folder to test_folder
+        for file in os.listdir(stock_data_folder):
+            src = os.path.join(stock_data_folder, file)
+            dst = os.path.join(test_folder, file)
+            shutil.move(src, dst)
+        
+        # Remove the now empty "Data Test" folder
+        print("[INFO] Removing empty 'Data Test' folder")
+        shutil.rmtree(stock_data_folder)
+
 
     
